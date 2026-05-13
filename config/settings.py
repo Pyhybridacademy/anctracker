e413757c116a43ea036882bb8108b677d8bfc1de
Pyhybridacademy <edikephilomena@gmail.com>
2026-05-13 21:51:08 +0100

    second

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 # Host configuration
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:8000,http://127.0.0.1:8000').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:8000,http://127.0.0.1:8000,https://anctracker.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
